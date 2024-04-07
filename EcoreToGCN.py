5b61b62a4704d87b71ca1d890400d5e0d72f2715
Constanze <constanzeohrem@aol.de>
2024-04-07 11:53:45 +0200

    rough outline works, loading xmi files and saving matrices in text files

--- EcoreToGCN.py
+++ EcoreToGCN.py
@@ -1,11 +1,39 @@
 from pyecore.resources import ResourceSet, URI
+from enum import Enum
 
-#load xmi instance
-rset = ResourceSet()
-resource = rset.get_resource(URI('Basic.ecore'))
-mm_root = resource.contents[0]
-rset.metamodel_registry[mm_root.nsURI] = mm_root
-resource = rset.get_resource(URI('../test_xmi/test.xmi'))
-typegraph_root = resource.contents[0]
+class xmiToGcnConverter:
+    def __init__(self, resource:ResourceSet):
+        self.typegraph_root = resource.contents[0] #we have the entire typegraph object here
+        self.node_matrix = [] #nxc feature matrix with n nodes and c features for each node: node type and identifier (e.g. name or location)
+        self.adjacency_matrix = [] #weighted graph edges
 
-print(typegraph_root)
+    def get_node_matrix(self):
+        return self.node_matrix
+    
+    def get_adjacency_matrix(self):
+        return self.adjacency_matrix
+    
+    def get_graph_name(self):
+        return self.typegraph_root.tName
+
+
+    class NodeLabels(Enum):
+        PACKAGE = 1
+        MODULE = 2
+        CLASS = 3
+        METHOD_DEFINITION = 4
+        METHOD_SIGNATURE = 5
+        METHOD = 6
+        PARAMETER = 7
+        CALL = 8
+
+    class EdgeLabels(Enum):
+        UNLABELED = 1
+        CONTAINS = 2
+        DEFINES = 3
+        ACCESSING = 4
+        CHILDCLASSES = 5
+        NEXT = 6
+        PREVIOUS = 7
+        TARGET = 8
+        SOURCE = 9
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 from ASTToEcore import ProjectEcoreGraph
 from pyecore.resources import ResourceSet, URI
+from EcoreToGCN import xmiToGcnConverter
 
 # repository_directory = '/mnt/volume1/mlexpmining/cloned_repos/'
 # output_directory = '/mnt/volume1/mlexpmining/ecore_graphs/'
 #repository_directory = '../'
 repository_directory = '../test_repository'
 output_directory = '../ecore_test'
-xmi_files = '../test_xmi'
+xmi_file_directory = '../test_xmi'
+gcn_input_directory = '../gcn_input'
 
 if __name__ == '__main__':
     if not os.path.exists(output_directory):
@@ -35,3 +37,40 @@ if __name__ == '__main__':
             # repositories.remove(repository)
     print("------------------------------------")
     print(f"Skipped {skip_counter} of {len(repositories)}.")
+    print("---------convert ecore graphs to matrix structure--------------")
+    #load xmi instance
+    skip_xmi = 0
+    xmi_files = os.listdir(xmi_file_directory)
+    if not os.path.exists(gcn_input_directory):
+        os.makedirs(gcn_input_directory)
+    rset = ResourceSet()
+    resource = rset.get_resource(URI('Basic.ecore'))
+    mm_root = resource.contents[0]
+    rset.metamodel_registry[mm_root.nsURI] = mm_root
+    for x, xmi_file in enumerate(xmi_files):
+        print(f"Progress: {x}/{len(xmi_files)}")
+        current_xmi_file = os.path.join(xmi_file_directory, xmi_file)
+        print(current_xmi_file)
+        resource = rset.get_resource(URI(f"{xmi_file_directory}/{xmi_file}"))
+        try:
+            project_gcn_input = xmiToGcnConverter(resource)
+            output_name = project_gcn_input.get_graph_name()
+            output_node_matrix = project_gcn_input.get_node_matrix()
+            output_adjacency_matrix = project_gcn_input.get_adjacency_matrix()
+
+            #save matrices in two text files
+            new_resource_nodes = open(f"{gcn_input_directory}/{output_name}_nxc.txt", "w+") 
+            for node in output_node_matrix: #iterate over slices?!
+                new_resource_nodes.write(output_node_matrix[node]) #i may need second for line to iterate over slice contents
+            new_resource_nodes.close()
+            new_resource_edges = open(f"{gcn_input_directory}/{output_name}_adjacency.txt", "w+")
+            for edge in output_adjacency_matrix:
+                new_resource_edges.write(output_adjacency_matrix[edge])
+            new_resource_edges.close()
+        except Exception as e:
+            print(e)
+            print(f"Problem with xmi file {xmi_file}. Skipping")
+            skip_xmi += 1
+    print("-----------------------------------")
+    print(f"Skipped {skip_xmi} of {len(xmi_files)}")
+
