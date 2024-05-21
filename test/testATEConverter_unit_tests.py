e0820452a46ecfa1fde2c01bd678e5b0fb828d90
Constanze <constanzeohrem@aol.de>
2024-05-21 18:33:30 +0200

    finished unit test of 1st converter, test passes

--- test/testATEConverter_unit_tests.py
+++ test/testATEConverter_unit_tests.py
@@ -15,8 +15,12 @@ rset.metamodel_registry[mm_root.nsURI] = mm_root
 resource = rset.get_resource(URI(f'{output_directory}/xmi_files/testRepo1.xmi'))
 typegraph = resource.contents[0]
 
+#initialize check and node types without name attribute --> check by counting their number
 check = False
 meth_def = 0
+meth_sig = 0
+param = 0
+call = 0
 
 #check number of packages and their names
 for p, pack in enumerate(typegraph.packages):
@@ -39,10 +43,32 @@ for mo, mod in enumerate(typegraph.modules):
         else:
             check == False 
             print('Module names not correct.')
+        #check edge between two modules and package
+        if mod_name == 'callFunc' or mod_name == 'testClass':
+            if mod.namespace.tName == 'my_package':
+                check = True
+            else:
+                check = False
+                print('Namespace of module should be the package. Error.')
         if hasattr(mod, 'contains'):
             for contained in mod.contains:
                 if contained.eClass.name == Types.METHOD_DEFINITION.value:
                     meth_def += 1
+                    #check call
+                    if hasattr(contained, 'accessing'):
+                        for ca in contained.accessing:
+                            if ca.eClass.name == Types.CALL.value:
+                                call += 1
+                                if ca.target.signature.method.tName == 'my_hello':
+                                    check = True
+                                else:
+                                    check = False
+                                    print('call target not correct.')
+                                if ca.source.signature.method.tName == 'greet_friend':
+                                    check = True
+                                else:
+                                    check = False
+                                    print('call source not correct.')
                 if contained.eClass.name == Types.CLASS.value:
                     if contained.tName == 'myTest':
                         check = True
@@ -68,6 +94,15 @@ for me, meth in enumerate(typegraph.methods):
         else:
             check = False
             print('Method names not correct.')
+        if hasattr(meth, 'signatures'):
+            for sig in meth.signatures:
+                meth_sig += 1
+                if hasattr(sig, 'parameters'):
+                    for parameter in sig.parameters:
+                        param += 1
+        else:
+            check = False
+            print('Method signature is missing.')
     else:
         check = False
         print('Number of methods not correct.')
@@ -77,8 +112,28 @@ if meth_def == 3:
     check = True
 else:
     check = False
-    print(meth_def)
     print('Number of method definitions not correct.')
+
+#check number of method signatures in total
+if meth_sig == 3:
+    check = True
+else:
+    check = False
+    print('Number of method signatures not correct.')
+
+#check number of parameters in total
+if param == 1:
+    check = True
+else:
+    check = False
+    print('Number of parameters not correct.')
+
+#check number of calls in total
+if call == 1:
+    check = True
+else:
+    check = False
+    print('Number of calls not correct.')
 
 #print result
 if check == True:
