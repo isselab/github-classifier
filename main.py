49cc3564db948f84ccca37fd25e5544c55900895
Constanze <constanzeohrem@aol.de>
2024-05-23 14:37:40 +0200

    ..

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ from PipelineUtils import prepare_dataset, create_matrix_structure, download_rep
 # repository_directory = '/mnt/volume1/mlexpmining/cloned_repos/'
 # output_directory = '/mnt/volume1/mlexpmining/ecore_graphs/'
 repository_list = '../random_sample.xls'
-repository_directory = 'D:/test_repos'
+repository_directory = 'D:/dataset_repos'
 output_directory = 'D:/tool_output'
 
 #repository_directory = 'test/unit_tests'
 #output_directory = '../output_tests'
 
+#maybe add download repos to prepare dataset?! 
+
 if __name__ == '__main__':
 
     try:
-        download_repositories(repository_directory, repository_list)
+        #download_repositories(repository_directory, repository_list)
         #create the graph dataset of the repositories
-        #prepare_dataset(repository_directory, output_directory)
+        prepare_dataset(repository_directory, output_directory)
         #create_matrix_structure(output_directory)
     except Exception as e:
         print(e)
