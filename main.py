d569e9d527781c63335c821a89a2ea5ef43e5027
Constanze <constanzeohrem@aol.de>
2024-06-02 12:16:13 +0200

    ..pipeline

--- main.py
+++ main.py
@@ -16,23 +16,28 @@ if __name__ == '__main__':
     try:
         # create the graph dataset of the repositories
         nodes, edges = prepare_dataset(repository_directory)
-        #load trained graph convolutional network model
-        model = torch.load('graph_classification_model.pt')
-        print(model)
-        print(nodes, nodes.size())
-        print(edges, edges.size())
-        output = model(nodes, edges, 1) #complains about missing batch
-        print(output)
+        if nodes is not None and edges is not None:
+            #load trained graph convolutional network model
+            model = torch.load('graph_classification_model.pt')
+            print(model)
+            print(nodes, nodes.size())
+            print(edges, edges.size())
+            output = model(nodes, edges, 1) #complains about missing batch
+            print(output)
     except Exception as e:
         print(e)
 
     print('----------------load dataset------------------')
     try:
-        #dataset = RepositoryDataset(f'{output_directory}/csv_files')
+        dataset = RepositoryDataset(f'{output_directory}/csv_files')
         print('Dataset size: ')
-        #print(dataset.__len__())
+        print(dataset.__len__())
         print('Number of classes: ')
-        #print(dataset.num_classes)
+        print(dataset.num_classes)
+        model = torch.load('graph_classification_model.pt')
+        for graph in dataset:
+            output = model(graph.x, graph.edge_index, 1) #how to save output? --> necessary to save it?
+            print(output) #maybe new func to get graph name?
     except Exception as e:
         print(e)
         print('The dataset cannot be loaded.')
