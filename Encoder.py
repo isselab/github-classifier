fe5b44ebbcc35164d2c44e31a1ab11af6e65dea8
Constanze <constanzeohrem@aol.de>
2024-06-12 18:20:02 +0200

    implemented one hot encoding for not mutually exclusive labels for graph dataset

--- Encoder.py
+++ Encoder.py
@@ -7,6 +7,7 @@ def label_encoding(labels, matrix):
     enc = label_encoder.transform(matrix)
     return enc
 
+'''one hot encoding for node features'''
 def one_hot_encoding(labels, matrix):
     #df = pd.DataFrame(matrix)
     #categorical_columns = df.select_dtypes(include=['object']).columns.to_list()
@@ -17,3 +18,36 @@ def one_hot_encoding(labels, matrix):
     matrix = pd.DataFrame(matrix)
     enc = one_hot_encoder.transform(matrix)
     return enc
+
+'''encodes the not mutually exclusive classes of graphs in the dataset,
+   encodes labels ['Application', 'Experiment', 'Framework', 'Library', 'Tutorial']'''
+def multiclass_one_hot_encoding(graph_classes, graph_list):
+    num_classes = len(graph_classes)
+    enc_labels = []
+    for label in graph_list:
+        enc = [0. for i in range(0, num_classes)]
+        if label == 'Application':
+            enc[0] = 1.0
+        if label == 'Experiment':
+            enc[1] = 1.0
+        if label == 'Framework':
+            enc[2] = 1.0
+        if label == 'Library':
+            enc[3] = 1.0
+        if label == 'Tutorial':
+            enc[4] = 1.0
+        if ',' in label:
+            multiple_labels = label.split(',')
+            for m_label in multiple_labels:
+                if m_label == 'Application' or m_label == ' Application':
+                    enc[0] = 1.0
+                if m_label == 'Experiment' or m_label == ' Experiment':
+                    enc[1] = 1.0
+                if m_label == 'Framework' or m_label == ' Framework':
+                    enc[2] = 1.0
+                if m_label == 'Library' or m_label == ' Library':
+                    enc[3] = 1.0
+                if m_label == 'Tutorial' or m_label == ' Tutorial':
+                    enc[4] = 1.0
+        enc_labels.append(enc)
+    return enc_labels
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from CustomDataset import RepositoryDataset
 from Pipeline import prepare_dataset
 import torch
 
-'''repository list is optional parameter, if you want to download the repositories automatically;
+'''prepare_dataset: repository list is optional parameter, if you want to download the repositories automatically;
     output_directory is required when more than one repository is going to be converted,
     if there is only one repository the output of the converter is saved in return variables and can be
-    piped into the gcn as input without needing to load the data from files'''
+    piped into the gcn as input without needing to load the data from files
 
-repository_list = '../dataset_co.xlsx'
+    RepositoryDataset: repository list is needed when using the dataset for training, the labels for
+    the graphs are loaded from that file, otherwise the graphs are loaded without labels'''
+
+repository_list = '../dataset_labeled.xlsx'
 repository_directory = 'D:/dataset_repos'
 output_directory = 'D:/data_output'
 path_to_model = 'graph_classification_model.pt'
@@ -16,27 +19,27 @@ if __name__ == '__main__':
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    try:
+    #try:
         # create the graph dataset of the repositories
-        nodes, edges = prepare_dataset(repository_directory, output_directory, repository_list)
-        if nodes is not None and edges is not None:
+        #nodes, edges = prepare_dataset(repository_directory, output_directory)
+        #if nodes is not None and edges is not None:
             #load trained graph convolutional network model
-            model = torch.load(path_to_model)
-            model.eval() #in pytorch doc!!
-            print(model)
-            print(nodes, nodes.size())
-            print(edges, edges.size())
+            #model = torch.load(path_to_model)
+           # model.eval() #in pytorch doc!!
+           # print(model)
+           # print(nodes, nodes.size())
+           # print(edges, edges.size())
             #if device == 'cuda':
                # nodes.to(device)
                 #edges.to(device)
-            output = model(nodes, edges) #complains about missing batch?
-            print(output)
-    except Exception as e:
-        print(e)
+            #output = model(nodes, edges) #complains about missing batch?
+            #print(output)
+    #except Exception as e:
+        #print(e)
 
     print('----------------load dataset------------------')
     try:
-        dataset = RepositoryDataset(f'{output_directory}/csv_files')
+        dataset = RepositoryDataset(f'{output_directory}/csv_files', repository_list)
         print('Dataset size: ')
         print(dataset.__len__())
         print('Number of classes: ')
