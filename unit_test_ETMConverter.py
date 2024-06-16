d0a44a3f803ea48b42329639037a4df84658b33f
Constanze <constanzeohrem@aol.de>
2024-06-16 18:20:35 +0200

    ..

--- unit_test_ETMConverter.py
+++ unit_test_ETMConverter.py
@@ -2,11 +2,18 @@ import unittest
 from AstToEcoreConverter import ProjectEcoreGraph
 from pyecore.resources import ResourceSet
 from NodeFeatures import NodeTypes
+from EcoreToMatrixConverter import EcoreToMatrixConverter
 
 class TestETMConv(unittest.TestCase):
     def test_etmconv(self):
-        output_dir = 'D:/unit_test'
         repo = 'test/unit_test_repo/testRepo1'
         resource_set = ResourceSet()
-        graph = ProjectEcoreGraph(resource_set, output_dir, repo, False)
+        graph = ProjectEcoreGraph(resource_set, repo, False)
         ecore_graph = graph.get_graph()
+        matrix = EcoreToMatrixConverter(ecore_graph, False)
+        node_features = matrix.get_encoded_node_matrix()
+        edges = matrix.get_adjacency_list()
+        #print(node_features, edges)
+
+
+unittest.main()
