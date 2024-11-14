6b7ad0da0ad3aaa95f7d55f1642679df7a46571b
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2024-11-14 12:45:27 +0100

    docs: Full docs for EdgeAttributes.py
    - added docs for class EdgeTypes

--- EdgeAttributes.py
+++ EdgeAttributes.py
@@ -3,6 +3,14 @@ from enum import Enum
 
 # types of relations between nodes in the type graph
 class EdgeTypes(Enum):
+    """
+     Enum representing the types of relations between nodes in a type graph.
+
+     This enumeration defines various types of edges that can exist between nodes,
+    providing a structured way to represent connections in a type graph. Each member
+    corresponds to a specific type of relationship, facilitating the understanding
+    and manipulation of the graph's structure.
+     """
     PARENT = "parent"
     SUBPACKAGE = "subpackage"
     MODULES = "modules"
