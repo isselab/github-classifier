fa57168d85c63ba6c0f746bbac78b3cb346b7e28
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2024-11-14 12:22:47 +0100

    docs: full docs for NodesFeatures.py
    - added docstring for class NodeTypes

--- NodeFeatures.py
+++ NodeFeatures.py
@@ -3,6 +3,12 @@ from enum import Enum
 
 # node types in the type graph
 class NodeTypes(Enum):
+    """
+       Enum representing different types of nodes in a type graph.
+
+       Each node type corresponds to a specific element in the type system,
+       providing a way to categorize and manage these elements programmatically.
+       """
     CALL = "TCall"
     CLASS = "TClass"
     MODULE = "TModule"
