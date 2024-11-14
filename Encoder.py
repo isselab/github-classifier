858243b3a904f62b95d45408afc65f35ea1e41a6
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2024-11-14 12:35:49 +0100

    docs: full docs for Encoder.py
    -create docstrings for def one_hot_encoding and multi_hot_encoding

--- Encoder.py
+++ Encoder.py
@@ -5,6 +5,16 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 def one_hot_encoding(labels, matrix):
+    """
+    Perform one-hot encoding on the given labels.
+
+    Args:
+        labels (array-like): The labels to be encoded, which will be transformed into a one-hot encoded format.
+        matrix (array-like): The matrix containing the original data, which is not used in the encoding process but is required for function signature.
+
+    Returns:
+        np.ndarray: A one-hot encoded array representing the input labels.
+    """
     one_hot_encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
     labels = pd.DataFrame(labels)
     one_hot_encoder.fit(labels)
@@ -18,6 +28,16 @@ def one_hot_encoding(labels, matrix):
 
 
 def multi_hot_encoding(graph_classes, graph_list):
+    """
+    Perform multi-hot encoding for a list of graph classes.
+
+    Args:
+        graph_classes: A list of unique class labels that can be assigned to nodes in the graph.
+        graph_list: A list of labels for each node, where each label can be a single class or a comma-separated string of multiple classes.
+
+    Returns:
+        list: A list of lists, where each inner list represents the multi-hot encoded vector for a corresponding label in `graph_list`.
+    """
     num_classes = len(graph_classes)
     enc_labels = []
     for label in graph_list:
