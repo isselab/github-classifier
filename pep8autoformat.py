31e5219771a1e617cc55933be1e215ec6954d8d3
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2024-12-03 14:54:57 +0100

    Update pep8autoformat.py

--- pep8autoformat.py
+++ pep8autoformat.py
@@ -1,21 +1,21 @@
 import autopep8
 
-def format_python_file(file_path):
+def format_python_file(path_to_file):
     try:
         # Read the current content of the file
-        with open(file_path, 'r') as file:
+        with open(path_to_file, 'r') as file:
             code = file.read()
 
         # Format the code using autopep8
         formatted_code = autopep8.fix_code(code)
 
         # Write the formatted code back to the file
-        with open(file_path, 'w') as file:
+        with open(path_to_file, 'w') as file:
             file.write(formatted_code)
 
-        print(f"Formatted '{file_path}' successfully.")
+        print(f"Formatted '{path_to_file}' successfully.")
     except Exception as e:
-        print(f"Error formatting '{file_path}': {e}")
+        print(f"Error formatting '{path_to_file}': {e}")
 
 
 if __name__ == "__main__":
